a36c85a4bea04087a60459aba3f0df7ad75b5bb3
m4vinayak <m4vinayak@gmail.com>
2023-10-23 17:45:02 +0530

    add question method

--- src/com/miniproject/quizapplication/QuizApplication.java
+++ src/com/miniproject/quizapplication/QuizApplication.java
@@ -1,26 +1,20 @@
 package com.miniproject.quizapplication;
+import java.io.IOException;
 import java.sql.SQLException;
-import com.miniproject.database.connection.DatabaseConnection;
-import com.miniproject.tables.InsertionIntoTable;
+
+import com.miniproject.tables.AddQuestionMethod;
 
 public class QuizApplication {
 	
 	public static void main(String[] args) {
 		
 		System.out.println("Start");
-
-		DatabaseConnection con = new DatabaseConnection();
-		try {
-			con.getConnetion();
-			System.out.println("Connection Established");
-		} catch (ClassNotFoundException | SQLException e) {
-			System.out.println(e);
-		}
 		
-		InsertionIntoTable insert= new InsertionIntoTable();
+		AddQuestionMethod addQuestionMethod = new AddQuestionMethod();
+		
 		try {
-			insert.insertIntoTable();
-		} catch (ClassNotFoundException | SQLException e) {
+			addQuestionMethod.addQuestionMethod();
+		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(e);
 		}
